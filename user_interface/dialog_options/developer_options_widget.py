596b0cd6f6f76609c3644899e0caa9558211def5
ゆんふぃ <yunfie.twitter+3@gmail.com>
2025-09-11 18:08:08 +0900

    Update developer_options_widget.py

--- user_interface/dialog_options/developer_options_widget.py
+++ user_interface/dialog_options/developer_options_widget.py
@@ -1,15 +1,17 @@
 from PyQt5.QtWidgets import QWidget, QFormLayout, QCheckBox, QLabel
 
 class DeveloperOptionsWidget(QWidget):
-    def __init__(self, parent=None, opts=None):
+    def __init__(self, parent=None, opts=None, i18n=None):
         super().__init__(parent)
+        self.i18n = i18n
         self.opts = opts or {}
         self.init_ui()
 
     def init_ui(self):
         layout = QFormLayout()
 
-        dev_label = QLabel("開発者向けオプション（慎重に使用してください）")
+        warning_text = self.i18n.t("developer_warning") if self.i18n else "開発者向けオプション（慎重に使用してください）"
+        dev_label = QLabel(warning_text)
         dev_label.setStyleSheet("color: red; font-weight: bold;")
         layout.addRow(dev_label)
 
