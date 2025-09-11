22b437feea7110e399e8a693b9f066695f67a5bc
ゆんふぃ <yunfie.twitter+3@gmail.com>
2025-09-11 18:07:55 +0900

    Update downloader_options_widget.py

--- user_interface/dialog_options/downloader_options_widget.py
+++ user_interface/dialog_options/downloader_options_widget.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QWidget, QFormLayout, QComboBox, QLabel
 
 class DownloaderOptionsWidget(QWidget):
-    def __init__(self, parent=None, current_downloader=""):
+    def __init__(self, parent=None, current_downloader="", i18n=None):
         super().__init__(parent)
+        self.i18n = i18n
         self.current_downloader = current_downloader
         self.init_ui()
 
@@ -11,10 +12,12 @@ class DownloaderOptionsWidget(QWidget):
 
         self.downloader_combo = QComboBox(self)
         self.downloader_combo.addItems(['', 'aria2c', 'avconv', 'axel', 'curl', 'ffmpeg', 'httpie', 'wget'])
+
         index = self.downloader_combo.findText(self.current_downloader)
         self.downloader_combo.setCurrentIndex(index if index >= 0 else 0)
 
-        layout.addRow("ダウンローダー (--downloader):", self.downloader_combo)
+        downloader_label = self.i18n.t("downloader_label") if self.i18n else "ダウンローダー (--downloader):"
+        layout.addRow(downloader_label, self.downloader_combo)
 
         self.setLayout(layout)
 
